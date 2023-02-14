78075703d3ea1f3fcf4e6e7a8f6c62e2dc95ada3
Asra Zareen <asrazareen0@gmail.com>
2023-02-14 17:50:34 +0530

    ADDED AUTHENTICATION

---
+++ routes/user.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const User = require("../model/user");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Secret = "INSTACLONEAPP";
+const cors = require("cors");
+app.use(cors());
+
+
+// app.get("/" , async (req,res) => {
+//      const users = await User.find()
+//      res.send(users)
+
+// })
+app.post("/register",
+    body("name").isAlphanumeric().isLength({ min: 4 }),
+    body("email").isEmail(),
+    body("password").isAlphanumeric().isLength({ min: 6 }),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                const error = errors.array()[0];
+                if (error.param == "email") {
+                    return res.status(400).json({
+                        message: "Invalid Email",
+                    });
+                } else if (error.param == "name") {
+                    return res.status(400).json({
+                        message: "Invalid Name",
+                    });
+                }
+                else {
+                    return res.status(400).json({
+                        message: "Invalid Password",
+                    });
+                }
+            }
+            const { name, email, password } = req.body;
+            const useremail = await User.findOne({ email });
+            if (useremail) {
+                return res.status(409).json({
+                    status: "Failed",
+                    message: "User already exists",
+                });
+            }
+
+            const username = await User.findOne({ name });
+            if (username) {
+                return res.status(409).json({
+                    status: "Failed",
+                    message: "Username already exists, please use another name",
+                });
+            }
+
+            bcrypt.hash(password,10,async(err,hash) => {
+                if (err) {
+                    return res.status(500).json({
+                      status: "Failed",
+                      message: err.message,
+                    });
+                  }
+                  await User.create({
+                    name,
+                    email,
+                    password: hash,
+                  })
+                  return res.status(200).json({
+                    status: "Success",
+                    message: "User registered successfully",
+                  });
+            })
+
+        }
+        catch(e) {
+            return res.status(500).json({
+                status: "Failed",
+                message: "Registration unsuccessful",
+              });
+        }
+    }
+)
+
+app.post("/login",
+body("email").isEmail(),
+async (req,res) => {
+    try{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid Email"
+            })
+        }
+        const {email,password} = req.body;
+      
+        const user = await User.findOne({ email })
+        // console.log(user)
+        if(!user){
+            return res.json({
+                errors:"Failed login",
+                message:"User not registered"
+            })
+        }
+        bcrypt.compare(password,user.password,(err,result) => {
+            if (err) {
+                return res.status(500).json({
+                  status: "Failed",
+                  message: err.message,
+                
+                });
+              }
+              if(result){
+                const token = jwt.sign(
+                    {
+                        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+                        data: user._id,
+                    },
+                    Secret
+                )
+                return res.json({
+                    status: "Succces",
+                    message: "Login successfully",
+                    token
+                })
+              }else{
+                return res.status(400).json({
+                    status: "Failed",
+                    message: "Wrong Password",
+                  });
+              }
+        })
+    }
+    catch(e){
+        return res.status(500).json({
+            status: "Failed",
+            message: "e.message",
+          });
+    }
+}
+)
+
+module.exports = app;
